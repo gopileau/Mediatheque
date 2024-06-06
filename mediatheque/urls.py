b3b8944311cbc022efc6ed75b5f75e9eff9986df
gopilo <vanessagonzalez99@hotmail.com>
2024-06-06 16:52:09 +0200

    Commented out emprunt_list URL

--- mediatheque/urls.py
+++ mediatheque/urls.py
@@ -20,8 +20,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),  # Inclure les URLs de l'application main
-    path('accounts/', include('django.contrib.auth.urls')),  # URLs d'authentification
+    path('', include('main.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
 
 
